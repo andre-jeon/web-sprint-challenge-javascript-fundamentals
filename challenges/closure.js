815643e4a2366a39ae167ad009a1d3ab0a035596
Myeongwon André Jeon <andrejeon@Myeongwons-MBP.fios-router.home>
2020-06-19 15:28:42 -0400

    everything complete

--- challenges/closure.js
+++ challenges/closure.js
@@ -27,20 +27,14 @@ and uses a counter to return the summation of that number.
 
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
 
-function summation() {
+function summation(number) {
 
   let count = 0;
-  
-  return function counter() {
-     return count++;
-     adderArray.push(count);
+
+  for(let i = 0; i <= number; i++) {
+    count = count + i;
   }
-
-  adderArray = [];
-
-  adderArray.reduce(function(accumulator, item){
-    return accumulator + item;
-  }, 0);
+  return count;
 }
 
 console.log(summation(4));
